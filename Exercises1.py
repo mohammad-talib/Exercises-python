280be4f8ecd01d3a46b870e58b17fdce0e71c0a6
mohammad-talib <mohammad.h.alhety@gmail.com>
2019-11-19 20:42:09 +0200

    Exercise1

---
+++ Exercises1.py
@@ -0,0 +1,108 @@
+#========================================Exercises 1 =========================================#
+print("Hello\n\tHello\n\t\tHello")
+
+#========================================Exercises 2 =========================================#
+print("Orange Academy \n"*20)
+
+#========================================Exercises 3 =========================================#
+x=float(1)
+y=float(2.8)
+z=float("3")
+w=float("4.2")
+print(x,y,z,w,sep="10")
+
+#========================================Exercises 4 =========================================#
+num1=int(input("Enter First Number"))
+num2=int(input("Enter second Number"))
+print(num1 * num2)
+
+#========================================Exercises 5 =========================================#
+number1=int(input("Enter First Number"))
+number2=int(input("Enter second Number"))
+number3=int(input("Enter therd Number"))
+number4=int(input("Enter forth Number"))
+number5=int(input("Enter Fivth Number"))
+print((number1 + number2 + number3 + number4 + number5)/5 )
+
+#========================================Exercises 6 =========================================#
+x=1
+y=2.8
+z=1j
+w="orange"
+A=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(w))
+print(type(A))
+
+#========================================Exercises 7 =========================================#
+x,y="Orange",1
+X1,y1=100,-10
+print(x)
+print(y)
+print(X1)
+print(y1)
+
+#========================================Exercises 8 =========================================#
+5x=10
+print(5x)
+X_q$="Orange"
+print(X_q$)
+A B=14
+print(A B)
+print("10"*10)
+
+#========================================Exercises 9 =========================================#
+x,y="Orange",1
+X1,y1=100,-10
+print(x)
+print(y)
+print(X1)
+print(y1)
+
+#========================================Exercises 10 =========================================#
+x5=10
+print(x5)
+X_q="Orange"
+print(X_q)
+AB=14
+print(AB)
+print(10*10)
+
+#========================================Exercises 11 =========================================#
+print("Hello, World!")
+print("Cheers, Mate!")#This Is The Program
+"""Testing
+The Code
+"""
+
+#========================================Exercises 12 =========================================#
+username = input("Enter Your Name! ")
+userage = int(input("Enter Your birthday! "))
+
+print("Hello "+username+" you will be 100 years in ", userage+100) 
+
+#========================================Exercises 13 =========================================#
+base=int(input("Enter the base of triangle "))
+height=int(input("Enter the height of triangle "))
+print(base * height * 1/2)
+
+#========================================Exercises 14 =========================================#
+x=11
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(abs(x*-1))
+print(int(x))
+print(float(x))
+print(divmod(x,y))
+print(pow(x,y))
+print(x**y)
+print(x>y)
+print(x==y)
+print(x != y)
